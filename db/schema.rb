ea661580e56e72a3c09eee17725185a6ee75225e
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-04-28 22:32:39 -0300

    Project model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20110429001656) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "Title"
+    t.string   "Subtitle"
+    t.string   "Description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
