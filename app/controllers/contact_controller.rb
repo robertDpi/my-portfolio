5b242cb64a9a15c9bb8bd2b8141ea57c8fbf7ce5
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-05-15 13:43:37 -0300

    Contact details

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,15 +5,13 @@ class ContactController < ApplicationController
 
   def contact_mail
     @contact = Contact.new(params[:contact])
-    puts @contact.inspect + "***************************"
+    # puts @contact.inspect
+    
     if @contact.valid?
       Notification.contact_me(@contact).deliver
-
-       puts "Lala" + @contact.inspect + "OK"
-      redirect_to root_path, :notice => "Email sent, we'll get back to you"
+      redirect_to root_path, :notice => "Thanks! It's nice to hear from you!"
     else
-      puts "Lala" + @contact.inspect + "ERROR"
-      render :new, :notice => "Errors"
+      render :new, :notice => "Please correct the errors below"
     end
   end
 end
