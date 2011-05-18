6cbfd9dac47973f622d291fa24d8ed4595aa2abc
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-05-17 22:12:15 -0300

    Contact form validations

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,12 @@ class Contact
   include ActiveModel::Conversion
 
   attr_accessor :name, :email, :body
-  validates_presence_of :name, :email, :body
+
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
+  validates :name, :presence => true
+  validates :email, :presence => true, :length => { :maximum => 100 }, :format => EMAIL_REGEX, :confirmation => true
+  validates :body, :presence => true
 
   def initialize(attributes = {})
     (attributes || {}).each do |name, value|
