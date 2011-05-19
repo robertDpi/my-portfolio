18f8decd1431cbacfc3b0b1af4059f8b8c3adb5a
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-05-19 01:03:59 -0300

    Helper for consistent page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,17 @@
 module ApplicationHelper
+  def page_title
+    if (@title)
+      @title + ' - Your portfolio site'
+    else
+      'Your portfolio site'
+    end
+  end
+
+  def page_heading(text)
+    @title = text
+    content_tag(:h2, text)
+  end
+  
   def error_messages_for(object)
   	render(:partial => "shared/error_messages", :locals => {:object => object})
 	end
