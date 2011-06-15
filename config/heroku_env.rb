99b35369df6f17b3085a82f4c804caae09aaf357
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-06-15 01:26:48 -0300

    Unversioning the local ENV vars file through gitignore

--- config/heroku_env.rb
+++
@@ -1,8 +0,0 @@
-# Developer specific local ENV vars needed for image upload and email sending with Heroku and AWS.
-# Do not check this file into version control once you enter your specific secret data.
-
-ENV['GMAIL_USERNAME']    = 'yoursecretdata'
-ENV['GMAIL_PASSWORD']    = 'yoursecretdata'
-ENV['S3_KEY']            = 'yoursecretdata'
-ENV['S3_SECRET']         = 'yoursecretdata'
-ENV['S3_BUCKET']         = 'yoursecretdata'
