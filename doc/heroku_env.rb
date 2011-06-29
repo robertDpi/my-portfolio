10279ea4b272ef793411fd85c23fc03dddbe4846
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-06-29 01:54:54 -0300

    Improved docs

--- doc/heroku_env.rb
+++ doc/heroku_env.rb
@@ -1,11 +1,10 @@
 # Developer specific local ENV vars needed for image upload and email sending with Heroku and AWS.
 # Do not check this file into version control once you enter your specific secret data.
 
-ENV['DOMAIN']            = 'your domain'
 ENV['GMAIL_USERNAME']    = 'youruser@gmail.com'
 ENV['GMAIL_PASSWORD']    = 'yourpassword'
 
 # Uncomment these if  you want to test with S3 locally
-# ENV['S3_KEY']            = 'yourS3key'
-# ENV['S3_SECRET']         = 'yourS3password'
-# ENV['S3_BUCKET']         = 'your-bucket'
+# ENV['S3_KEY'] = 'yourS3key'
+# ENV['S3_SECRET'] = 'yourS3password'
+# ENV['S3_BUCKET'] = 'your-bucket'
