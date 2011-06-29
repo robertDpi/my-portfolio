a178899cc2e7f55e78d92015cd49f4d36a7c9182
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-06-29 10:50:30 -0300

    Improvements to passwords controller

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -3,15 +3,11 @@ class PasswordsController < ApplicationController
 
   def edit
     @user = current_user
-    render :template => 'passwords/edit', :locals => {:resource => @user, :resource_name => User }
   end
 
   def update
     @user = current_user
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
-
-    if @user.save
+    if @user.update_attributes(params[:user])
       sign_in(@user, :bypass => true)
       redirect_to root_path, :notice => "Password updated!"
     else
