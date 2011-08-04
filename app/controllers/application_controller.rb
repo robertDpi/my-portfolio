05c52ddaa5cc7c503bbd641b2e9cf0b455e0ff52
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-08-03 22:18:09 -0300

    Project navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :set_navigation
+
+  private
+  def set_navigation
+    @project_list = Project.all
+  end
+  #TODO: Code Review
 end
