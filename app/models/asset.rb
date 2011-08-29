eb11e3415935be83973e2c1134d08ea1ae1a2ef2
Veronica Rebagliatte <veronica.rebagliatte@moove-it.com>
2011-08-28 23:30:33 -0300

    Paperclip settings

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,4 @@
 class Asset < ActiveRecord::Base
   belongs_to :project
-  has_attached_file :asset, :styles => {:thumb => "100x100!", :large => "618x480>"}
+  has_attached_file :asset, :styles => {:thumb => "100x100!", :large => "618x480>"}.merge(Rails.configuration.paperclip_storage)
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,12 +1,7 @@
 class Project < ActiveRecord::Base
-  attr_accessible :title, :subtitle, :description, :assets_attributes  
+  attr_accessible :title, :subtitle, :description, :cover, :assets_attributes  
   validates :title, :presence => true, :length => { :maximum => 50 }
-  has_attached_file :cover,
-                    {
-                      :styles => {
-                          :thumb => "170x133"
-                      },
-                    }.merge(Rails.configuration.paperclip_storage)
+  has_attached_file :cover, :styles => {:thumb => "170x133"}.merge(Rails.configuration.paperclip_storage)
   has_many :assets, :dependent => :destroy
   accepts_nested_attributes_for :assets, :allow_destroy => true
 end
